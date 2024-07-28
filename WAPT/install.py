6fa587b2a4ccf1ea537276af0086e968411136d2
MK-1407 <mayank140706@gmail.com>
2024-07-28 13:45:22 +0530

    Fixed infinite Downloading

--- WAPT/install.py
+++ WAPT/install.py
@@ -12,12 +12,12 @@ def get_package_url(package_name, version):
     package_url = Url()
     for package in sources:
         if package[0] == package_name:
-            package_url.url = package[1]
-            package_url.status = 200
-            return package_url
+            if package[3] == version or package[2]==version:
+                package_url.url = package[1]
+                package_url.status = 200
         else:
             package_url.status = 404
-            return package_url        
+    return package_url        
 
 def install(package_name, version='latest'):
     package_url : Url = get_package_url(package_name=package_name, version=version)
--- WAPT/search.py
+++ WAPT/search.py
@@ -4,9 +4,10 @@ import csv
 def search(package_name):
     sources_file = open("C:/Users/Mayank/Programming/wapt/sources_list.csv")
     sources = list(csv.reader(sources_file))
+    packages = []
     for package in sources:
-        if package_name == package[0]:
-            return {"name":package[0],"url":package[1],"version":package[3]}
+        if package_name.lower() == package[0].lower():
+            packages.append({"name":package[0],"url":package[1],"version":package[3]})
         else:
             return "Package not found"
-    
+    return packages
